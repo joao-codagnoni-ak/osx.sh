050ec6430572db0e05d53e784af80b5f4031c465
Rafael Henrique Tibães <r@fael.nl>
2018-05-03 10:16:42 -0300

    a lot of brews

---
+++ install.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+echo "First things first: make sure to install XCode building tools..."
+
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+brew install wget curl \
+    screen \
+    vim \
+    ranger \
+    fish \
+    htop
+
+brew install qt
+brew install opencv ffmpeg 
+brew install boost clang-format cmake ninja pkg-config
+
+brew cask install font-fira-code \
+    font-fira-mono \
+    font-fira-mono-for-powerline \
+    font-fira-sans
+brew cask install julia 
+brew cask install fork 
+brew cask install visual-studio-code 
+brew cask install android-studio
+brew cask install qt-creator
+brew cask install telegram slack whatsapp skype
+brew cask install microsoft-office
+brew cask install google-chrome brave 
+brew cask install steam
+brew cask install onedrive
+brew cask install docker
+brew cask install keka
+brew cask install mpv vlc
+brew cask install mactex-no-gui
+brew cask install xquartz
+
+curl -L https://get.oh-my.fish | fish
+omf install budspencer
+omf theme budspencer
+
+echo "Grab on AppStore: "
+echo "Spark, Magnet"
+echo "Graphic, Pixelmator, iMovie, GarageBand, Delineato"
